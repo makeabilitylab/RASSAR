1aac0886b47f5eafd4fc4da1f4fa541c1c38abf5
Xia Su <61726267+xiasu@users.noreply.github.com>
2022-07-11 15:16:05 -0700

    Initial Commit

---
+++ RetroAccess App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RetroAccess App
+//
+//  Created by Xia Su on 7/11/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
